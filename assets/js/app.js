ffe36e85a2195d12153eb7489d1d610ea7f563d5
memedia <evans.scott.r@gmail.com>
2021-06-02 12:53:13 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -426,7 +426,7 @@ var featureLayer = L.geoJson(null, {
 $(document).ready(function () {
   $.ajax({
     type: "GET",  
-    url: "http://cors.io/?https://erica.altschul.info/542Tutorial_03.xml", //"https://0489.cxtsoftware.net/CxtWebService/CxtWebServiceRest.svc/rest/process/ImportWS/PUNCHTHROUGH/4756",
+    url: "https://cors.io/?https://erica.altschul.info/542Tutorial_03.xml", //"https://0489.cxtsoftware.net/CxtWebService/CxtWebServiceRest.svc/rest/process/ImportWS/PUNCHTHROUGH/4756",
     crossDomain: true,
     dataType: "xml",
     xhrFields: {
